12dbee995f1e309be9b562637b475e6680f60933
Parham Alvani <parham.alvani@gmail.com>
2025-04-09 10:02:10 +0000

    feat: add jwt parsing

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.2
 
 require (
 	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/jwt/v2 v2.7.3 // indirect
 	github.com/nats-io/nats.go v1.41.0 // indirect
 	github.com/nats-io/nkeys v0.4.10 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/nats-io/jwt/v2"
 	"github.com/nats-io/nats.go"
 )
 
-const natsURL = "nats://auth:auth@127.0.0.1:4222"
+const (
+	natsURL  = "nats://auth:auth@127.0.0.1:4222"
+	nkeySeed = "SAANDLKMXL6CUS3CP52WIXBEDN6YJ545GDKC65U5JZPPV6WH6ESWUA6YAI"
+)
 
 func handler(msg *nats.Msg) {
 	log.Printf("Received authentication request on subject: %s, reply: %s", msg.Subject, msg.Reply)
 
-	log.Println(string(msg.Data))
+	rc, err := jwt.DecodeAuthorizationRequestClaims(string(msg.Data))
+	if err != nil {
+		log.Println("Error", err)
+	}
+
+	userId := rc.ConnectOptions.Username
+
+	claims := jwt.NewUserClaims(rc.UserNkey)
+	claims.Audience = "CHAT"
+	claims.Name = userId
+	claims.Permissions = jwt.Permissions{
+		Pub: jwt.Permission{
+			Allow: jwt.StringList{
+				"$JS.API.INFO", // General JS Info
+
+				// Chat permisions
+				fmt.Sprintf("chat.*.%s", userId),            // Publishing chat messages for this user id
+				"$JS.API.STREAM.INFO.chat_messages",         // Getting info on chat_messages stream
+				"$JS.API.CONSUMER.CREATE.chat_messages.>",   // Creating consumers on chat_messages stream
+				"$JS.API.CONSUMER.MSG.NEXT.chat_messages.>", // Creating consumers on chat_messages stream
+
+				// Workspace permissions
+				"$JS.API.DIRECT.GET.KV_chat_workspace.>",        // Gets from workspace KV
+				"$JS.API.STREAM.INFO.KV_chat_workspace",         // Info about workspace KV
+				"$JS.API.CONSUMER.CREATE.KV_chat_workspace.*.>", // Creating consumers/watchers on workspace KV
+			},
+		},
+	}
+
+	log.Println(rc)
 }
 
 func main() {
